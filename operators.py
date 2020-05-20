716766eeb61eb3c08d567bc5b8dee8d64e8f17c9
Wildcard357 <64090700+Wildcard357@users.noreply.github.com>
2020-05-20 08:27:48 +0000

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,86 @@
+# There are various operators in python
+
+#ASSIGNMENT OPERATORS
+
+a=5 
+#this operator assigns the value 5 to A
+
+a+=3
+#this is an increment operarator same as a=a+3
+
+a-=3
+#this is a decrement operator same as a=a-3
+
+#ARITHMETIC OPERATORS
+#for them to work values must assigned first
+
+a=2
+b=3
+
+a+b 
+#this is an addition operator
+
+a-b
+#this is a subtraction operator
+
+a*b
+#this is a multiplication operator
+
+a/b
+#this is a division operator
+
+a%b
+#this is a modulus operator
+
+a**b
+#this is an exponentiation operator
+
+a//b
+#this is a floor division operator
+
+#COMPARISON OPERATORS
+
+a==b
+#this tests if a is equal to b
+
+a!=b
+#this tests if a is not equal to b
+
+a>b
+#this tests if a is greater than b
+
+a<b
+#this tests if a is less than b
+
+a>=b
+#this tests if a is greater or equal to b
+
+a<=b
+#this tests if a is less or equal to b
+
+#LOGICAL OPERATORS
+
+a<4 and a<10
+#this returns True if both of the statements are true
+
+a<4 or x<3
+#this returns True if one of the statements is true
+
+not(a<4 and a<10)
+#this reverses the result. It returns False if the result was True
+
+#IDENTITY OPERATORS
+
+x is y
+#this returns true if both variables are the same object
+
+x is not y
+#this returns true if both variables are not the same object
+
+#MEMBERSHIP OPERATORS
+
+a in b
+#this returns True if the value a is present in the object b
+
+a not in b
+#this returns True if the value a is not present in the object b
---
+++ variales.py
@@ -0,0 +1,17 @@
+#python variables
+#let's start with int
+
+#int
+a=3 #a at this point is an intager
+
+#float
+a=2.3 #a at this point is a float because of the decimal point
+
+#complex
+a=2k3 #a at this point is a complex because of the combination of numbers and letters
+
+#strig
+a="John" #a at this point is string meaning its a word
+
+#boolean 
+a=True #a at this point is a boolean since its value is True
